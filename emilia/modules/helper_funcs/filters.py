4fc7310305817af4ad8a8517f364af6d88054853
Nizar Rian <garudaindonesia710@gmail.com>
2021-01-05 07:53:23 +0700

    Update filters.py

--- emilia/modules/helper_funcs/filters.py
+++ emilia/modules/helper_funcs/filters.py
@@ -5,6 +5,12 @@ from emilia import SUPPORT_USERS, SUDO_USERS
 
 
 class CustomFilters(object):
+    class _Supporters(BaseFilter):
+        def filter(self, message: Message):
+            return bool(message.from_user and message.from_user.id in SUPPORT_USERS)
+
+    support_filter = _Supporters()
+
     class _Sudoers(BaseFilter):
         def filter(self, message: Message):
             return bool(message.from_user and message.from_user.id in SUDO_USERS)
